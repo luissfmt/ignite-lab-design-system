178bc7e7b458a88442cd1bb175bcfe2a47489dda
luissfmt <luis_melotavares@hotmail.com>
2022-10-14 15:11:55 -0300

    Initial commit

---
+++ src/components/Text.stories.tsx
@@ -0,0 +1,45 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Text, TextProps } from "./Text";
+
+export default {
+    title: 'Components/Text',
+    component: Text,
+    args: {
+        children: 'Lorem ipsum',
+    }
+} as Meta<TextProps>;
+
+export const Default: StoryObj<TextProps> = {};
+
+export const Small: StoryObj<TextProps> = {
+    args: {
+        size: 'sm'
+    }
+};
+
+export const Large: StoryObj<TextProps> = {
+    args: {
+        size: 'lg'
+    }
+};
+
+export const CustomComponent: StoryObj<TextProps> = {
+    args: {
+        asChild: true,
+        children: (
+            <p>Text with P tag</p>
+        )
+    },
+    argTypes: {
+        children: {
+            table: {
+                disable: true 
+            }
+        },
+        asChild: {
+            table: {
+                disable: true
+            }
+        }
+    }
+};
